2c35934ad44e1f700832eea5f3594257140dc2ef
Dan Hsu <hsu86110@gmail.com>
2021-09-03 16:02:33 +0800

    feat: 1. add light 2. add RWD, window event listener

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,28 @@ import './index.css';
 import * as THREE from 'three';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(0xeeeeee, 1.0);
+renderer.shadowMap.enabled = true;
 
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00});
+
+let pointLight = new THREE.PointLight(0xaaaaaa);
+pointLight.position.set(0,0,6);
+// pointLight.lookAt(scene.position);
+scene.add(pointLight);
+
+const geometry = new THREE.BoxGeometry(1,1,1);
+const material = new THREE.MeshLambertMaterial({ color: 0x00ff00});
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-camera.position.z = 5;
+
+camera.position.set(0,0,6);
 
 const animate = function () {
   requestAnimationFrame(animate);
@@ -26,3 +35,9 @@ const animate = function () {
 }
 
 animate();
+
+window.addEventListener('resize', function(){
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
